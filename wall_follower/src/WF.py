8aafdfb876ae47b0f4f4505a6a7e4b97e1a481e0
Ssduckk <dlwhdrlf0619@naver.com>
2021-12-03 14:45:05 +0900

    setting range and angle

--- wall_follower/src/WF.py
+++ wall_follower/src/WF.py
@@ -1,6 +1,7 @@
 #!/home/pi/.pyenv/versions/rospy385/bin/python
 
 import rospy
+import math
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
@@ -8,9 +9,24 @@ from sensor_msgs.msg import LaserScan
 class WallFollower:
     def __init__(self, publisher):
         self.publisher = publisher
+        self.count = 0
 
     def lds_callback(self, scan):
-        pass
+        turtle_vel = Twist()
+        forward = (average(scan.ranges[0:18]) + average(scan.ranges[342:359])) / 2
+        right = average(scan.ranges[265:275])
+        right_forward = average(scan.ranges[300:310])
+        right_behind = average(scan.ranges[230:240])
+        pointright = scan.ranges[270]
+        self.count += 1
+        print('Forward', forward, '\nR:', right, 'fR:', right_forward,
+              'bR:', right_behind, '\npR:', pointright)
+        print(self.count)
+
+
+
+def average(some_list):
+    return sum(some_list)/len(some_list)
 
 
 def main():
