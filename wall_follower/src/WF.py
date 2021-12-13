4f7d722e4004a6e2ee263b7e50c5cee12d423b36
Ssduckk <dlwhdrlf0619@naver.com>
2021-12-13 21:43:20 +0900

    functionalize lds callback class

--- wall_follower/src/WF.py
+++ wall_follower/src/WF.py
@@ -12,59 +12,57 @@ class WallFollower:
         self.count = 0
 
     def lds_callback(self, scan):
-        turtle_vel = Twist()
-        forward = (average(scan.ranges[0:18]) + average(scan.ranges[342:359])) / 2
-        right = average(scan.ranges[265:275])
-        right_forward = average(scan.ranges[300:310])
-        right_behind = average(scan.ranges[230:240])
-        pointright = scan.ranges[270]
+        self.turtle_vel = Twist()
+        self.forward = (average(scan.ranges[0:18]) + average(scan.ranges[342:359])) / 2
+        self.right = average(scan.ranges[265:275])
+        self.right_forward = average(scan.ranges[300:310])
+        self.right_behind = average(scan.ranges[230:240])
+        self.pointright = scan.ranges[270]
 
         # angular z = rad/s -> 초당 약 57도 회전을 의미한다.
 
-        if forward > 0.25:
-            if right > 0.5 and right_forward > 0.5 and right_behind > 0.5:
-                turtle_vel.linear.x = 0.18
-                turtle_vel.angular.z = 0
-
-            elif 0.18 < right < 0.23:
-                if right_forward > right_behind:
-                    turtle_vel.linear.x = 0.15
-                    turtle_vel.angular.z = -0.3
-                elif right_behind > right_forward:
-                    turtle_vel.linear.x = 0.15
-                    turtle_vel.angular.z = 0.3
-
-            elif right_forward < 0.15:
-                turtle_vel.linear.x = 0.1
-                turtle_vel.angular.z = 0.5
-
-            # elif right < 0.2 or right_forward < 0.2 or right_behind < 0.2:
-            #     if right_forward > right_behind:
-            #        turtle_vel.linear.x = 0.1
-            #        turtle_vel.angular.z = -0.5
-            #     elif right_behind > right_forward:
-            #        turtle_vel.linear.x = 0.1
-            #        turtle_vel.angular.z = 0.5
-
-            elif 0.5 < right_forward or right_forward == 0:
-                turtle_vel.linear.x = 0.1
-                turtle_vel.angular.z = -1.5
-
-            else:
-                turtle_vel.linear.x = 0.18
-                turtle_vel.angular.z = 0
+        if self.forward > 0.25:
+            self.follow_wall()
         else:
-            turtle_vel.linear.x = 0.1
-            turtle_vel.angular.z = 2.2
+            self.meet_wall()
 
-        self.publisher.publish(turtle_vel)
+        self.publisher.publish(self.turtle_vel)
 
         self.count += 1
         if self.count % 10 == 0:
-            print('Forward', forward, '\nR:', right, 'fR:', right_forward,
-                  'bR:', right_behind, '\npR:', pointright)
+            print('Forward', self.forward, '\nR:', self.right, 'fR:', self.right_forward,
+                  'bR:', self.right_behind, '\npR:', self.pointright)
             print(self.count)
 
+    def follow_wall(self):
+        if self.right > 0.5 and self.right_forward > 0.5 and self.right_behind > 0.5:
+            self.turtle_vel.linear.x = 0.18
+            self.turtle_vel.angular.z = 0
+
+        elif 0.18 < self.right < 0.23:
+            if self.right_forward > self.right_behind:
+                self.turtle_vel.linear.x = 0.15
+                self.turtle_vel.angular.z = -0.3
+            elif self.right_behind > self.right_forward:
+                self.turtle_vel.linear.x = 0.15
+                self.turtle_vel.angular.z = 0.3
+
+        elif self.right_forward < 0.15:
+            self.turtle_vel.linear.x = 0.1
+            self.turtle_vel.angular.z = 0.5
+
+        elif 0.5 < self.right_forward or self.right_forward == 0:
+            self.turtle_vel.linear.x = 0.1
+            self.turtle_vel.angular.z = -1.5
+
+        else:
+            self.turtle_vel.linear.x = 0.18
+            self.turtle_vel.angular.z = 0
+
+
+    def meet_wall(self):
+        self.turtle_vel.linear.x = 0.1
+        self.turtle_vel.angular.z = 2.2
 
 def average(some_list):
     return sum(some_list)/len(some_list)
